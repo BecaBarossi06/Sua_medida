f15f5beaaa17b727bec74b445d46f55f2e0a2dfc
Rebeca Barossi <95308519+BecaBarossi06@users.noreply.github.com>
2024-10-01 13:18:12 -0300

    Add files via upload
    deploy

--- conexao.php
+++ conexao.php
@@ -1,8 +1,14 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_error',1);
+
+
+
 $servername = "localhost";  // ou o nome do seu servidor
-$username = "root";  // nome de usuário do banco
-$password = "";  // senha do banco (pode estar vazia se você estiver usando XAMPP)
-$dbname = "suamedida";  // nome do banco de dados
+$username = "ifhostgru_suamedida";  // nome de usuário do banco
+$password = "ifspgru@2022";  // senha do banco (pode estar vazia se você estiver usando XAMPP)
+$dbname = "ifhostgru_suamedida";  // nome do banco de dados
 
 // Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
