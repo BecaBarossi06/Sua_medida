4746d2ca96ff6b0abc5a0dc0db73b3b3d658a380
Rebeca Barossi <95308519+BecaBarossi06@users.noreply.github.com>
2024-12-04 11:01:14 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 $servername = "144.217.39.54";  // IP do servidor do banco de dados
 $username = "hostdeprojetos";  // Usuário
 $password = "ifspgru@2022";  // Senha
-$dbname = "hostdeprojetos";  // Nome do banco
+$dbname = "hostdeprojetos_suamedida";  // Nome do banco
 
 // Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
