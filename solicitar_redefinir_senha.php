a2f80283965544c40ed6540a934c693ee8570548
BecaBarossi06 <rebecabarossi@gmail.com>
2024-12-04 12:48:31 -0300

    versao nova

--- solicitar_redefinir_senha.php
+++
@@ -1,6 +0,0 @@
-<!-- reset_password_request.php -->
-<form method="POST" action="processa_solicitar.php">
-    <label for="email">Digite seu e-mail:</label>
-    <input type="email" name="email" required>
-    <button type="submit">Enviar link de redefinição</button>
-</form>
